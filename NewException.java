ef5230989698865bd14f9c87b94411c8dafadebf
Yutaka Haraguchi <caio_2h@hotmail.com>
2015-03-23 01:12:09 -0300

    Update NewException.java

--- NewException.java
+++ NewException.java
@@ -1,4 +1,4 @@
-//Classe de exceção "personalizada"
+//Classe de excecao "personalizada"
 //que herda da classe Exception
 public class NewException extends Exception{
     NewException (String message) {
